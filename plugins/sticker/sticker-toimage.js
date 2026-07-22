ca63637d9c6de5f8460b4877555dc6f6ccba3724
Neykoor <cafecafirexos@gmail.com>
2026-07-22 08:55:15 -0700

    Update sticker-toimage.js

--- plugins/sticker/sticker-toimage.js
+++ plugins/sticker/sticker-toimage.js
@@ -3,7 +3,7 @@ import fs from 'fs'
 import os from 'os'
 import path from 'path'
 import { randomUUID } from 'crypto'
-import { resolveFfmpegPath } from '../lib/ffmpegPath.js'
+import { resolveFfmpegPath } from '../../lib/ffmpegPath.js'
 
 ffmpeg.setFfmpegPath(resolveFfmpegPath())
 
